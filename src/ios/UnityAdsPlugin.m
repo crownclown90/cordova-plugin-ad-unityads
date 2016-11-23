227d3cd7c580750512469054a0fb7b9a4a58ea35
crownclown90 <khale-el@hotmail.co.uk>
2016-11-23 12:36:36 +0000

    Update UnityAdsPlugin.m

--- src/ios/UnityAdsPlugin.m
+++ src/ios/UnityAdsPlugin.m
@@ -85,32 +85,7 @@ static NSString *TEST_REWARDED_VIDEO_AD_PLACEMENT_ID = @"rewardedVideoZone";
 - (void) _setLicenseKey:(NSString *)email aLicenseKey:(NSString *)licenseKey {
 	self.email = email;
 	self.licenseKey_ = licenseKey;
-	
-	//
-	NSString *str1 = [self md5:[NSString stringWithFormat:@"cordova-plugin-: %@", email]];
-	NSString *str2 = [self md5:[NSString stringWithFormat:@"cordova-plugin-ad-unityads: %@", email]];
-	NSString *str3 = [self md5:[NSString stringWithFormat:@"com.cranberrygame.cordova.plugin.: %@", email]];
-	NSString *str4 = [self md5:[NSString stringWithFormat:@"com.cranberrygame.cordova.plugin.ad.unityads: %@", email]];
-	if(licenseKey_ != Nil && ([licenseKey_ isEqualToString:str1] || [licenseKey_ isEqualToString:str2] || [licenseKey_ isEqualToString:str3] || [licenseKey_ isEqualToString:str4])){
-		self.validLicenseKey = YES;
-		NSArray *excludedLicenseKeys = [NSArray arrayWithObjects: @"xxx", nil];
-		for (int i = 0 ; i < [excludedLicenseKeys count] ; i++) {
-			if([[excludedLicenseKeys objectAtIndex:i] isEqualToString:licenseKey]) {
-				self.validLicenseKey = NO;
-				break;
-			}
-		}
-	}
-	else {
-		self.validLicenseKey = NO;
-	}
-	if (self.validLicenseKey)
-		NSLog(@"valid licenseKey");
-	else {
-		NSLog(@"invalid licenseKey");
-		//UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Alert" message:@"Cordova UnityAds: invalid email / license key. You can get free license key from https://play.google.com/store/apps/details?id=com.cranberrygame.pluginsforcordova" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
-		//[alert show];
-	}
+	self.validLicenseKey = YES;
 }
 
 - (NSString*) md5:(NSString*) input {
@@ -131,15 +106,6 @@ static NSString *TEST_REWARDED_VIDEO_AD_PLACEMENT_ID = @"rewardedVideoZone";
 	self.videoAdPlacementId = videoAdPlacementId;
 	self.rewardedVideoAdPlacementId = rewardedVideoAdPlacementId;
 	self.isTest = isTest;
-	
-	if (!validLicenseKey) {
-		if (arc4random() % 100 <= 1) {//0 ~ 99		
-			self.gameId = TEST_GAME_ID;
-			self.videoAdPlacementId = TEST_VIDEO_AD_PLACEMENT_ID;
-			self.rewardedVideoAdPlacementId = TEST_REWARDED_VIDEO_AD_PLACEMENT_ID;
-		}
-	}
- 
 	//https://unityads.unity3d.com/help/Documentation%20for%20Publishers/Integration-Guide-for-iOS
 	//https://unityads.unity3d.com/help/Documentation%20for%20Publishers/Integration-Guide-for-Unity
 	//http://www.unityads.co.kr/?page_id=866#guide_ios
